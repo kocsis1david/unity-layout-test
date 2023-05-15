65bec0af37a3f15e5a62fc9d3d946bafa9675aba
Dávid Kocsis <kocsis1david@windowslive.com>
2023-05-15 10:16:19 +0200

    init

---
+++ Assets/SineMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.localPosition;
+        pos.y = Mathf.Sin(Time.time * 3) * 0.5f + 1.0f;
+        transform.localPosition = pos;
+    }
+}
